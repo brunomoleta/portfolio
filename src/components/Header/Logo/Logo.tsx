d58510531b71d33e606aebcf60e7fa44fe62397d
Bruno Moleta <brunomoleta@pm.me>
2024-04-04 16:48:34 -0300

    Improve accessibility and update portrait description
    The commit introduces an aria-label and title for the Logo component to improve website accessibility. The alt text for the HeroPortrait also was simplified, providing a cleaner and more concise description of the image.

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import { StyledLogo } from "./Logo.style";
 
 function Logo() {
-  return <StyledLogo href="/">B.</StyledLogo>;
+  return (
+    <StyledLogo
+      aria-label="Ir para página inicial"
+      title="Ir para página inicial"
+      href="/"
+    >
+      B.
+    </StyledLogo>
+  );
 }
 
 export default Logo;
