1dc6942357187f496e92417af011f3e352bf0b50
Bruno Moleta <brunomoleta@pm.me>
2024-05-06 19:36:29 -0300

    :card_file_box: Set colors objects
    Set the colors for the light and dark themes.

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export const LIGHT_COLORS = {
+  "--color-text": "var(--cool-grey-90)",
+  "--color-text-secondary": "var(--cool-grey-80)",
+  "--color-text-third": "var(--color-text-third)",
+  "--color-background": "hsl(210deg, 20%, 98%)",
+  "--teal-primary": "var(--teal-70)",
+  "--button-background": "var(--cool-grey-5)",
+  // '--teal-secondary': 'hsl(345deg 100% 50%)',
+};
+
+export const DARK_COLORS = {
+  "--color-text": "var(--cool-grey-5)",
+  "--color-text-secondary": "var(--cool-grey-20)",
+  "--color-text-third": "var(--cool-grey-30)",
+  "--color-background": "hsl(230deg, 20%, 8%)",
+  "--teal-primary": "var(--teal-20)",
+  "--button-background": "black",
+  // '--color-secondary': 'hsl(210deg 100% 70%)',
+};
