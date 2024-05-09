a685cbdddbf62e1dbcf7cecded47023945aede64
Bruno Moleta <brunomoleta@pm.me>
2024-05-09 12:07:37 -0300

    :recycle: refactor/Increase media query size
    The moment where the project Piece switches from flex to grid in increased by 300px for better design.

--- src/components/SingleProject/SingleProjTour/TourPiece/TourPiece.style.ts
+++ src/components/SingleProject/SingleProjTour/TourPiece/TourPiece.style.ts
@@ -4,9 +4,9 @@ import styled from "styled-components";
 export const Piece = styled.div`
   font-size: var(--font-body-sm-1);
 
-  @media (min-width: 750px) {
-    display: grid;
-    grid-template-columns: 20svw auto;
-    gap: var(--s1);
-  }
+    @media (min-width: 1050px) {
+        display: grid;
+        grid-template-columns: 20svw auto;
+        gap: var(--s1);
+    }
 `;
