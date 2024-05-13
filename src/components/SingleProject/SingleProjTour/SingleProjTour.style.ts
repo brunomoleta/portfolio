286ad98630a96730e7304b26f92d27e07322b5e1
Bruno Moleta <brunomoleta@pm.me>
2024-05-13 12:29:19 -0300

    :lipstick: feat: change line at project
    The line in between project images was short and now expands from end to end of the screen.
    It uses background-colors and gaps.

--- src/components/SingleProject/SingleProjTour/SingleProjTour.style.ts
+++ src/components/SingleProject/SingleProjTour/SingleProjTour.style.ts
@@ -1,12 +1,12 @@
 "use client";
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.section`
   display: flex;
   flex-flow: column;
   margin-block: var(--s2);
 
   border-block-end: 4px solid var(--teal-50);
 
-  gap: var(--s3);
+  gap: var(--s-3);
 `;
