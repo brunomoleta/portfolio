981e1665d7615a4c5655a6dcd2c14d39659cc992
Bruno Moleta <brunomoleta@pm.me>
2024-05-13 12:31:02 -0300

    :broom: clean: reformat code

--- src/components/SingleProject/SingleProjTour/TourPiece/TourPiece.style.ts
+++ src/components/SingleProject/SingleProjTour/TourPiece/TourPiece.style.ts
@@ -3,10 +3,12 @@ import styled from "styled-components";
 
 export const Piece = styled.div`
   font-size: var(--font-body-sm-1);
+  display: grid;
+  padding-block: var(--s0) var(--s-5);
+  background-color: var(--color-background);
 
-    @media (min-width: 1050px) {
-        display: grid;
-        grid-template-columns: 20svw auto;
-        gap: var(--s1);
-    }
+  @media (min-width: 1050px) {
+    grid-template-columns: 20svw auto;
+    gap: var(--s1);
+  }
 `;
