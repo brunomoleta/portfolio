6f3833504314b5a34f833ec8f3db45169c77620d
Bruno Moleta <brunomoleta@pm.me>
2024-05-21 16:58:47 -0300

    :label: type:Zustand states

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -2,13 +2,13 @@ import { ICommon } from "@/types/types";
 
 export interface Utils {
   step: number;
-  
+
   nextStep: () => void;
   prevStep: () => void;
   returnFirstStep: () => void;
-  
+
   loading: boolean;
-  setLoading: ()=> void;
+  setLoading: () => void;
 
   project: null | ICommon;
   setProject: (project: ICommon) => void;
