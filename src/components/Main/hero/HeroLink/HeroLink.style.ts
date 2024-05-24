00233ea3730ad335d71e1fb539011438826d9f47
Bruno Moleta <brunomoleta@pm.me>
2024-05-24 16:49:07 -0300

    :dizzy: style:Hero Links

--- src/components/Main/hero/HeroLink/HeroLink.style.ts
+++ src/components/Main/hero/HeroLink/HeroLink.style.ts
@@ -3,43 +3,75 @@ import styled from "styled-components";
 import Link from "next/link";
 
 export const ListItem = styled(Link)`
-  text-align: center;
-  padding: var(--s-4) var(--s1);
-
-  border: 2px solid var(--cool-grey-90);
-  border-radius: 16px;
-
-  font-weight: var(--weight-bold);
-
-  width: fit-content;
-
-  background-color: var(--color-background);
-
-  -webkit-box-shadow: 4px 4px 12px 0 rgba(172, 241, 200, 0.8);
-  -moz-box-shadow: 4px 4px 12px 0 rgba(172, 241, 200, 0.8);
-  box-shadow: 4px 4px 12px 0 rgba(172, 241, 200, 0.8);
-
-  &:hover {
-    background-color: var(--button-hover);
-    outline-color: var(--teal-60);
-    color: var(--teal-80);
+    --outer: var(--s0);
+    --padding: var(--s-3);
+    --inner: calc(var(--outer) - var(--padding));
+    
     border: 2px solid transparent;
-  }
+    border-radius: var(--outer);
+    margin:0;
+    
+    display: grid;
+    
+    font-weight: var(--weight-bold);
 
-  &:active {
-    border-color: var(--teal-60);
-  }
 
-  &:focus {
-    border-color: var(--teal-60);
-  }
+    background-color: var(--color-background);
 
-  @media (prefers-reduced-motion: no-preference) {
-    transition:
-      color 300ms,
-      background-color 500ms;
+    -webkit-box-shadow: inset 2px 2px 4px hsl(0deg 0% 0% / 0.33);
+    -moz-box-shadow: inset 2px 2px 4px hsl(0deg 0% 0% / 0.33);
+    box-shadow: inset 2px 2px 4px hsl(0deg 0% 0% / 0.33);
+
+    overflow: hidden;
+
+    
     &:hover {
-      transition: background-color 250ms ease-in-out;
+        background-color: var(--color-line);
+        
+        outline-color: var(--teal-primary);
+        color: var(--teal-80);
+        
+        border: 2px solid transparent;
+
+        -webkit-box-shadow: inset 2px 2px 4px var(--teal-primary);
+        -moz-box-shadow: inset 2px 2px 4px var(--teal-primary);
+        box-shadow: inset 2px 2px 4px var(--teal-primary);
     }
-  }
+
+    &:active {
+        border: 2px solid var(--teal-60);
+    }
+
+    &:focus {
+        border: 2px solid var(--teal-60);
+    }
+
+    @media (prefers-reduced-motion: no-preference) {
+        transition: color 400ms,
+        background-color 500ms,
+        border 600ms;
+        &:hover {
+            transition: color 200ms ease-in-out,
+            background-color 250ms ease-in-out,
+            border 300ms ease-out;
+            &>div{
+                transition: background-color 500ms ease-in-out;
+            }
+        }
+    }
+    
+    &>div{ 
+    transition: background-color 300ms ease-in;
+    box-shadow:
+            2px 2px 8px
+            var(--teal-hover);
+    //background: inherit;
+    border-radius: var(--inner);
+    padding: var(--s-3) var(--s0);
+            background-color: inherit;
+        &:hover {
+            background-color: var(--button-hover);
+        }
+    }
 `;
+
